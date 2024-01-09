eb0b27b5a8d6477f84a8e7cf79b90afe468f5c7c
Harley <77483822+BigMark824@users.noreply.github.com>
2024-01-09 19:12:25 +1100

    Update RCCServiceArbiter.cpp

--- RCCServiceArbiter/RCCServiceArbiter.cpp
+++ RCCServiceArbiter/RCCServiceArbiter.cpp
@@ -30,7 +30,8 @@ bool IsPortOpen() {
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serverAddress.sin_port = htons(10836);
+    // change this to your soap port pal
+    serverAddress.sin_port = htons(99999);
 
     int connectResult = connect(sock, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress));
 
